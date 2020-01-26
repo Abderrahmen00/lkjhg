37f045b4f9b3088d0f4ed4918635fc8edc8f6d8f
Abderrahmen00 <54540199+Abderrahmen00@users.noreply.github.com>
2020-01-26 16:14:48 +0100

    Add files via upload

---
+++ src/components/Container.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ListComponent from './ListComponent';
+const Container = () =>{return (
+    <div>
+        <h1>hello world</h1>
+        <ListComponent />
+    </div>
+)}
+
+export default Container;
+
---
+++ src/components/ListComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ListItem from './ListItem';
+
+const ListComponent = () =>{
+    return(
+    <div>
+        <h1>hi</h1>
+        <ListItem />
+    </div>
+    )
+};
+
+export default ListComponent;
